312b54f9c0b203173593435efa5ef20574319c58
Dhif216 <dhif_mouadh@hotmail.fr>
2025-11-09 16:19:16 +0200

    Update base URL for custom domain and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 💡 CRITICAL FIX: Tell Vite to prefix all asset paths with the repository name
-  base: '/arem/', 
-  
+  base: '/', // Using custom domain, so we use root path
   plugins: [react()],
 });
